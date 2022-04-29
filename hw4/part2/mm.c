d918bda02fd1c17feebe91051fec9f344f4bcbf2
OEmiliatanO <emiliaistruelove@gmail.com>
2022-04-29 17:00:47 +0800

    update mm.c

--- hw4/part2/mm.c
+++ hw4/part2/mm.c
@@ -1,8 +1,40 @@
 #include "mm.h"
+#include <unistd.h>
 
-void *mymalloc(size_t size)
+struct header
 {
-	return NULL;
+	size_t size = 0;
+	node *nex = nullptr;
+}*now = nullptr;
+
+
+size_t align(size_t require)
+{
+	return (require + (unit - 1)) & ~(unit - 1);
+}
+
+void *mymalloc(size_t raw_size)
+{
+	size_t size = align(sizeof(struct header) + raw_size);
+
+	if (now == nullptr)
+	{
+		now = (header *)sbrk(0);
+		sbrk(size);
+		now->size = size;
+		now->nex = now;
+		return (void *)(now + 1);
+	}
+	
+	header *p = now;
+	for (; p->nex != now; p = p->nex)
+	{
+		if (p->size - sizeof(struct header) >= size)
+			return (void *)(p + 1);
+	}
+	p = (header *)sbrk(0);
+	sbrk(size);
+	return (void *)(p + 1);
 }
 
 void myfree(void *ptr)
